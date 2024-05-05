67d83e9d7024e814eaf221fad8c1097124fa04ad
Nelson Nobre <technobre@gmail.com>
2024-05-05 15:30:07 +0100

    test: Validate log extensions

--- src/LoggerExtensions.cs
+++ src/LoggerExtensions.cs
@@ -5,6 +5,11 @@ namespace PowerUtils.AspNetCore.ErrorHandler
 {
     internal static class LoggerExtensions
     {
+        public static void Debug(this ILogger logger, string message)
+            => logger.LogDebug(
+                "[ERROR HANDLER] > {Message}", message);
+
+
         public static void Error(this ILogger logger, Exception exception, string message)
             => logger.LogError(
                 exception,
@@ -13,23 +18,17 @@ namespace PowerUtils.AspNetCore.ErrorHandler
         public static void Error(this ILogger logger, Exception exception, ErrorProblemDetails problemDetails)
             => logger.LogError(
                 exception,
-                "[ERROR HANDLER] > Request: '{Request}', StatusCode: '{StatusCode}'", _sanitizeInput(problemDetails.Instance), problemDetails.Status);
+                "[ERROR HANDLER] > Request: '{Request}', StatusCode: '{StatusCode}'", _sanitize(problemDetails.Instance), problemDetails.Status);
 
         public static void Error(this ILogger logger, Exception exception, ErrorProblemDetails problemDetails, string message)
             => logger.LogError(
                 exception,
-                "[ERROR HANDLER] > Request: '{Request}', StatusCode: '{StatusCode}' > {Message}", _sanitizeInput(problemDetails.Instance), problemDetails.Status, message);
-
-
-
-        public static void Debug(this ILogger logger, string message)
-            => logger.LogDebug(
-                "[ERROR HANDLER] > {Message}", message);
+                "[ERROR HANDLER] > Request: '{Request}', StatusCode: '{StatusCode}' > {Message}", _sanitize(problemDetails.Instance), problemDetails.Status, message);
 
 
 
         // Function to sanitize user input before logging
-        private static string _sanitizeInput(string input)
-            => input?.Replace(Environment.NewLine, "");
+        private static string _sanitize(string input)
+            => input?.Replace(Environment.NewLine, " ") ?? "";
     }
 }
